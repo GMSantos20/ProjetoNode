cb9f5cc3f06358f2f934fd996120deaf58fb1bb0
GMSantos20 <santos220924@gmail.com>
2025-12-03 11:18:23 -0300

    rota registro

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,1 +1,25 @@
-import router from "express"
+import express from "express"
+import User from "../models/Users";
+
+const router =express.Router();
+
+
+router.post('/registro', async (req,res)=>{
+    const{usuario , senha} =req.body;
+    try{
+        const novoUsuario= await User.create({usuario , senha})
+        res.status(201).json({
+            message:'usuario registrado (Arq Rotas)',
+            usuario:novoUsuario.usuario
+        });
+    }catch (error){
+        if(error.code === 11000){
+            return res.status(409).json({message :'Usuario ja esta cadastrado'})
+        }
+        res.status(500).json({message:'Erro interno!!'})
+
+    }
+
+    
+    
+})
